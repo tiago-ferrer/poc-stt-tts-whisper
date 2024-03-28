3f350a381fc428d4d3fa1b23e7e8fe054f08817f
Tiago Ferrer <tiago.ferrer@preventsenior.com.br>
2024-03-28 18:55:51 -0300

    feat: adicionando gemini e removendo files desnecessarios apos gerar o resumo

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+import os
+
+from gemini import Gemini
 from transcriber import Transcriber
 from youtube_downloader import YouTubeDownloader
 
 videos = [
-    ("fiapDemo","https://www.youtube.com/watch?v=UC7WaqrIY2U")
+    ("git","https://www.youtube.com/watch?v=DqTITcMq68k")
 ]
 
 downloader = YouTubeDownloader(videos)
 downloader.download_videos()
 
+# Usage
+gemini = Gemini(os.getenv('GOOGLE_API_KEY'), 'gemini-pro')
+
 for video in videos:
     name, url = video
-    Transcriber().transcribe_audio_to_srt(audio_path=f'({name}.mp3)',output_file_path=f'({name}.txt)')
+    Transcriber().transcribe_audio_to_srt(audio_path=f'{name}.mp3',output_file_path=f'{name}.txt')
+    gemini.generateAbstract(f'{name}.txt', f'{name}-resumo.txt')
+    os.remove(f'{name}.mp3')
+    os.remove(f'{name}.txt')
